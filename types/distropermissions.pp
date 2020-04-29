8598b103d2bcba8d76f318d18197bd036237081e
Jan Vansteenkiste <jan@vstone.eu>
2020-04-29 15:15:21 +0200

    Adjust types for the new structure
    We need to keep api and /public/ permissions separated

--- types/distropermissions.pp
+++ types/distropermissions.pp
@@ -1,5 +1,5 @@
-# Represent possible permissions that can be configured for a published distribution or a prefix pool. Either a keyword or an array of usernames.
-type Aptly_profile::DistroPermissions = Variant[
-  Aptly_profile::AllowFromKeywords,
-  Array[String, 1],
-]
+# Represents pre-parsed permissions for a certain distribution within a prefix.
+type Aptly_profile::DistroPermissions = Struct[{
+  public => Optional[Aptly_profile::AllowFromPermissions],
+  api    => Optional[Aptly_profile::AllowFromPermissions],
+}]

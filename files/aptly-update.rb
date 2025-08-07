4068233813402b29a2b1b4d1dd8a24d647048e79
Jan Vansteenkiste <jan.vansteenkiste@ugent.be>
2025-08-07 09:53:48 +0200

    feature(aptly-update): Add mirror only option to aptly-update script

--- files/aptly-update.rb
+++ files/aptly-update.rb
@@ -89,6 +89,10 @@ OptionParser.new do |opts|
     @options[:verbose] = v
   end
 
+  opts.on('-m', '--[no-]mirror-only', 'Only create and update mirrors') do |m|
+    @options[:mirror_only] = m
+  end
+
   opts.on_tail('-h', '--help', 'Show this message') do
     puts opts
     exit
@@ -138,29 +142,31 @@ end
 
 @publish_errors = []
 
-if ARGV.empty?
-  Dir.glob('publish.d/*.yaml').each do |f|
-    begin
-      publish_yaml_config(f)
-    rescue Aptly::RunError => e
-      @publish_errors << [f, e]
+unless @options[:mirror_only]
+  if ARGV.empty?
+    Dir.glob('publish.d/*.yaml').each do |f|
+      begin
+        publish_yaml_config(f)
+      rescue Aptly::RunError => e
+        @publish_errors << [f, e]
+      end
+    end
+  else
+    ARGV.each do |req|
+      freq = req.gsub('/', '__')
+      begin
+        publish_yaml_config("publish.d/#{freq}.yaml")
+      rescue Aptly::RunError => e
+        @publish_errors << [freq, e]
+      end
     end
   end
-else
-  ARGV.each do |req|
-    freq = req.gsub('/', '__')
-    begin
-      publish_yaml_config("publish.d/#{freq}.yaml")
-    rescue Aptly::RunError => e
-      @publish_errors << [freq, e]
-    end
-  end
-end
 
-begin
-  @aptly.cleanup
-rescue Aptly::RunError => e
-  @publish_errors << ['cleanup', e]
+  begin
+    @aptly.cleanup
+  rescue Aptly::RunError => e
+    @publish_errors << ['cleanup', e]
+  end
 end
 
 unless @mirror_errors.empty?
